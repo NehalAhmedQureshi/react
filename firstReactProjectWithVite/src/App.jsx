c64518872f0da78d7e9754282871dfa29cac82c3
Nehal <m.nehalq125@gmail.com>
2024-08-08 05:32:35 -0700

    set p tag

--- firstReactProjectWithVite/src/App.jsx
+++ firstReactProjectWithVite/src/App.jsx
@@ -2,7 +2,9 @@
 function App() {
 
   return (
-    <><h1>Nehal Ahmed Qureshi</h1><p>SMIT Batch 11</p>Roll Number:WMA-213857<p></p></>
+    <><h1>Nehal Ahmed Qureshi</h1>
+    <p>SMIT Batch 11</p>
+    <p>Roll Number:WMA-213857</p></>
   )
 }
 
