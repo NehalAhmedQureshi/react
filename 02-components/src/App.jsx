74618a0629a6af0844e10348cc6cdc72c9a64057
Nehal <m.nehalq125@gmail.com>
2024-08-28 22:08:20 -0700

    update 02 components

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,66 +1,18 @@
-import StudentData from "./components/studentData"
-import Avatar from "./components/avatar"
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
 
-  // const studentData = [
-  //   {
-  //     name: "nehal",
-  //     rollNo: 213857,
-  //     batch: 11,
-  //     teacher: "Sir Bilal",
-  //   },
-  //   {
-  //     name: "arsalan",
-  //     rollNo: 214857,
-  //     batch: 12,
-  //     teacher: "Sir Ahmer",
-  //   },
-  //   {
-  //     name: "arib",
-  //     rollNo: 224857,
-  //     batch: 12,
-  //     teacher: "Sir Ahmer",
-  //   },
-  // ]
-
   return (
-    <div style={{
-      width: "100%",
-      display: "flex",
-      justifyContent: "center",
-      flexDirection: "column",
-    }}>
+    <div className="main">
+      <form className='avatarForm'>
 
-      <h1>User Profile</h1>
+        <input type="text" className="avatarName" />
+        <input type="file" className='avatarImg' />
 
-      <StudentData 
-        name="Nehal"
-        rollNo="213857"
-        batch="11"
-        teacher="sir bilal"
-      />
-
-      <Avatar
-        img="https://media.istockphoto.com/id/2021524839/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%84%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D1%8B-%D0%B8-%D0%B8%D0%BD%D0%B2%D0%B5%D1%81%D1%82%D0%B8%D1%86%D0%B8%D0%B8-%D0%B2-%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81-%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7%D0%B8%D1%80%D1%83%D0%B9%D1%82%D0%B5-%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%BA%D0%B8-%D1%8D%D0%BA%D0%BE%D0%BD%D0%BE%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B3%D0%BE-%D1%80%D0%BE%D1%81%D1%82%D0%B0-%D0%B4%D0%BB%D1%8F-%D0%BF%D1%80%D0%B8%D0%BD%D1%8F%D1%82%D0%B8%D1%8F.jpg?s=612x612&w=0&k=20&c=k4l63g9YFCzjadPc7khYxo6NyqqHZdJxVO9kLvrobKE="
-        name="def"
-        title="this is me"
-        bgcolor="grey"
-      />
-
-      <Avatar
-        img="https://media.istockphoto.com/id/1560326952/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D1%81%D0%B2%D0%B5%D1%82%D0%BB%D0%B0%D1%8F-%D1%81%D1%82%D1%80%D0%B5%D0%BB%D0%BA%D0%B0-%D0%B2%D0%B2%D0%B5%D1%80%D1%85-%D0%BF%D0%BE-%D1%81%D0%B5%D1%82%D0%BA%D0%B5-%D1%82%D0%B5%D0%BC%D0%BD%D0%B0%D1%8F-%D1%84%D0%BE%D0%BD%D0%BE%D0%B2%D0%B0%D1%8F-%D0%B8%D0%BB%D0%BB%D1%8E%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F-%D0%BA%D0%BE%D0%BF%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D1%81%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%B0.jpg?s=612x612&w=0&k=20&c=NVFX_Sk4P30VEoQfkOJXEBOmJsuwuJv1377QOtJ6dL4="
-        name="Arsalan"
-        title="----------------"
-        bgcolor="lightSalmon"
-      />
-
-      <Avatar
-        img="https://media.istockphoto.com/id/1419539600/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81-%D0%BF%D1%80%D0%B5%D0%B7%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F-%D0%B8-%D0%BC%D1%83%D0%B6%D1%87%D0%B8%D0%BD%D0%B0-%D0%BD%D0%B0-%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA%D0%B5-%D0%B2-%D0%BA%D0%BE%D1%80%D0%BF%D0%BE%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D0%B9-%D0%BA%D0%BE%D0%BD%D1%84%D0%B5%D1%80%D0%B5%D0%BD%D1%86%D0%B8%D0%B8-%D0%B8%D0%BB%D0%B8-%D0%BE%D1%84%D0%B8%D1%81%D0%BD%D0%BE%D0%B5.jpg?s=612x612&w=0&k=20&c=d-qxnh6z6X073mDJ-JW8CpvDm4OpvSs-_g9jJXcMaZg="
-        name="Arib"
-        title="---------------"
-        bgcolor="lavender"
-      />
+      </form>
 
     </div>
   )
--- 02-components/src/components/avatar.jsx
+++ 02-components/src/components/avatar.jsx
@@ -1,31 +1,11 @@
-function Avatar({ img, name, title, bgcolor,textColor }) {
-    return (
-        <div style={{
-            border: "2px solid black",  
-            display: "flex",
-            borderRadius: "150px",
-            margin: "auto",
-            padding: "10px",
-            marginBottom: "10px",
-            paddingLeft: "20px",
-            paddingRight: "80px",
-            gap: "40px",
-            backgroundColor: bgcolor ? bgcolor : "violet",
-            color: textColor ? textColor : "black"
-        }}>
-            <img style={{ 
-                border: "2px solid black",
-                width:"100px",
-                borderRadius:"50%", }}
-                src={img} alt="user avatar" />
-            <div style={{ 
-                flexGrow: 1 }}>
-                <h2 className="username">{name}</h2>
-                <h3 className="title">{title}</h3>
+function Avatar({name , profileImg }){
+    return(
+        <div className="avatar">
+            <img src={profileImg} className="avatarImg" />
+            <div className="content">
+                <div className="avatarName">{name}</div>
             </div>
         </div>
     )
 }
-
-
 export default Avatar
